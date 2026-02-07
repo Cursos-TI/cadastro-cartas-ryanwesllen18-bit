aa227eb7df84893b6be0e577acb5a3c27850bf48
ryanwesllen18-bit <ryanwesllen18@gmail.com>
2026-02-07 01:14:50 +0000

    printf("primeira versão do super trunfo.");

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
+// Desafio Super trunfo - Cadastro das cartas
+// a função principal int main é o inicial do programa e necessária para a excecução do código do jogo, finalizando com o return 0 para mostrar que o programa finalizou corretamente.
+//a entrada de dados é feita para efetuar as perguntas necessarias para o cadastro das cartas com a função scanf e a saída e estrutura printf para mostrar as informações cadastradas e estruta da formula.
+int main(){
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+//declaração da variaveis: populção, área, pib, número de pontos turísticos
+// população utiliza int pois é um numero inteiro se refere a pessoas
+// área utiliza float pois locais podem ter medidas com casas decimais
+//pib utiliza float pois o valor pode ter casa decimais
+//numero de pontos turísticos utiliza int pois é um numero inteiro
+// entrada e sáida de dados são necessarias para o cadastro das cartas, sempre utilizando informações claras e objetivas
+  int populacao;
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+printf("Digite a população da cidade: ");
+scanf("%d", &populacao);
+printf("População cadastrada é de: %d habitantes\n", populacao);
 
-  // Área para entrada de dados
+float area;
 
-  // Área para exibição dos dados da cidade
+printf("Digite a área da cidade: ");
+scanf("%f", &area);
 
-return 0;
-} 
+printf("Área cadastrada é de: %.2f km\n", area);
+
+
+float pib;
+
+printf("Digite o pib da cidade: ");
+scanf("%f", &pib);
+
+printf("PIB cadastrado é de: %f\n", pib);
+
+
+int numerodepontosturisticos;
+
+printf("Digite o numero de pontos de pontos turísticos da cidade: ");
+scanf("%d", &numerodepontosturisticos);
+
+printf("O número de pontos turísticos cadastrados é de: %d\n", numerodepontosturisticos);
+
+    return 0;
+}
